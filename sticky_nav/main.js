da24fc2e38f6c481c37e6798c77fd487e47e42db
pnowak <piotr.nowak.25.02.1978@gmail.com>
2017-01-04 21:42:09 +0100

    fixed nav and header

--- sticky_nav/main.js
+++ sticky_nav/main.js
@@ -1,5 +1,14 @@
 document.addEventListener('scroll', function (e) {
-	console.log(e);
-	var h1 = document.querySelector('h1');
-	
+	var nav = document.querySelector('nav'),
+		head = document.querySelector('header'),
+	    navToTop = nav.offsetTop,
+	    headFromTop = parseInt(window.getComputedStyle(head, null).getPropertyValue('height'));
+
+	if ((window.pageYOffset >= navToTop) && (window.pageYOffset >= headFromTop)) {
+		nav.classList.add('fixed');
+		head.classList.add('fixed');
+	} else {
+		nav.classList.remove('fixed');
+		head.classList.remove('fixed');
+	}
 }, false);
